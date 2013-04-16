6ca13dfbc3fdb67d2b9ecca042ac1daab29e288a
Frank Pimenta <frank.pimenta@swisscom.com>
2013-04-16 11:08:25 +0200

    $ migration_generator: install generator tests

--- test/generators/test_install_generator.rb
+++ test/generators/test_install_generator.rb
@@ -3,14 +3,45 @@ require "generators/mini_test/install/install_generator"
 
 class TestInstallGenerator < GeneratorTest
 
-  def test_install_generator
+  def test_assert_test_helper_is_installed
+    assert_output(/create  test\/test_helper.rb/m) do
+      MiniTest::Generators::InstallGenerator.start
+    end
+  end
+
+  def test_assert_migration_test_helper_exists
+    assert_output(/create  test\/test_helper.rb/m) do
+      MiniTest::Generators::InstallGenerator.start
+    end
+    assert File.exists?("test/migration_test_helpers.rb"), "migration test helper does not exist"
+  end
+
+  def test_assert_output_is_rendered_on_install
+    assert_output(/create  test\/test_helper.rb/m) do
+      MiniTest::Generators::InstallGenerator.start
+    end
+  end
+
+  def test_assert_test_helper_exists
     assert_output(/create  test\/test_helper.rb/m) do
       MiniTest::Generators::InstallGenerator.start
     end
     assert File.exists?("test/test_helper.rb"), "test helper file does not exist"
-    assert File.exists?("test/migration_test_helpers.rb"), "migration test helper does not exist"
+  end
+
+  def test_assert_test_helper_requires_rails_test_help
+    assert_output(/create  test\/test_helper.rb/m) do
+      MiniTest::Generators::InstallGenerator.start
+    end
     contents = File.read "test/test_helper.rb"
     assert_match(/require "rails\/test_help"/m, contents)
+  end
+
+  def test_assert_test_helper_requires_minitest_rails
+    assert_output(/create  test\/test_helper.rb/m) do
+      MiniTest::Generators::InstallGenerator.start
+    end
+    contents = File.read "test/test_helper.rb"
     assert_match(/require "minitest\/rails"/m, contents)
   end
 
